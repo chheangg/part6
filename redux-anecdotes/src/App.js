34aa35eb985cc300bf8d84df2d4fb64ad9872d20
chheangg <balldremz@gmail.com>
2022-12-24 20:04:28 +0700

    Complete 6.8 on modularizing project

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,31 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { voteFor} from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
 
 import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
-  const anecdotes = useSelector(state => state.sort((prev, next) => prev.votes > next.votes ? -1 : prev.votes === next.votes ? 0 : 1))
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(voteFor(id))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,41 @@
+import { useDispatch, useSelector } from "react-redux"
+import { voteFor } from "../reducers/anecdoteReducer"
+
+const Anecdote = ({content, votes, id, eventHandler}) => {
+  return (
+    <div>
+      <div>
+        {content}
+      </div>
+      <div>
+        has {votes}
+        <button onClick={eventHandler}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state.sort((prev, next) => prev.votes > next.votes ? -1 : prev.votes === next.votes ? 0 : 1))
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(voteFor(id))
+  }
+
+  return (
+    <>
+      {anecdotes.map(anecdote => 
+        <Anecdote 
+          key={anecdote.id} 
+          id={anecdote.id} 
+          votes={anecdote.votes} 
+          content={anecdote.content} 
+          eventHandler={() => vote(anecdote.id)} 
+        />
+        )}
+    </>
+  )
+}
+
+export default AnecdoteList
